c2bfad8f66fd62398afee7e00735fbdce2906bb2
mananuf <bmananaf@gmail.com>
2025-12-09 04:10:30 +0100

    chore: registers all modules in nimbu-core

--- crates/nimbu-core/src/lib.rs
+++ crates/nimbu-core/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod ids;
+pub mod task;
+// pub mod dag;
+// pub mod job;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use ids::*;
+pub use task::*;
+// pub use dag::*;
+// pub use job::*;
