5e78340d00ccae69f270f935e1a87e5f59d8b31e
mananuf <bmananaf@gmail.com>
2025-12-09 04:08:13 +0100

    feat: adds job and task id generators

---
+++ crates/nimbu-core/src/ids.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Hash, Eq)]
+pub struct JobId(String);
+
+impl JobId {
+    pub fn new() -> Self {
+        Self(Uuid::new_v4().to_string())
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Hash, Eq)]
+pub struct TaskId(String);
+
+impl TaskId {
+    pub fn new() -> Self {
+        Self(Uuid::new_v4().to_string())
+    }
+}
